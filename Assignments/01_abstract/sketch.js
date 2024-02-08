7492afd6eb90963853697e3e9e4cbad09b958627
Andrew37-sketch <ad9671a@american.edu>
2024-02-08 12:53:08 -0500

    created 2 examples of 3 shapes fitting into larger shapes with different colors

---
+++ Assignments/01_abstract/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas (1000,800);
+ // put setup code here
+}
+background()
+function draw() {
+    background(400,100,220)
+    fill(248, 252, 3);
+    rect(100,100,300,300)
+    fill(252, 161, 3);
+    ellipse(250, 250, 280, 280);
+    fill(2255, 3, 3); 
+    triangle(250,115,150,350,350,350)
+    fill(3, 252, 136); 
+    triangle(650,100,800,400,500,400)
+    fill(3, 173, 252);
+    ellipse(650, 300, 180, 180);
+    fill(137, 5, 245);
+    rect(585,230,130,130)
+}
+
